d6c2f0eb6c8a12ac46da469f56444a0a060e23c6
Arzumy MD <hello@ar.zu.my>
2011-11-25 12:31:00 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mefindfood::Application.config.secret_token = 'abddd9b3dc65533a3b48c77d7947817025df6ab6649e92d124889b6f67e4744b6fe7bba5e5a8e89b10cd0b759a15034817969ff7675be3bc4a431bfe2446a350'
